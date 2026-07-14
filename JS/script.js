32bccf5a6d904118e45ae9143b42fb42cd9721f2
Essa-Cortez <essawcortez@outlook.com>
2026-07-13 18:53:31 -0600

    Se agregó el carrusel de productos al index y scrip y se creo la hora de estilos style_Essaw

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,20 @@
+//------------------En caso de dudas revisar todas que dejé en mi codigo Atte: Essaw Cortez
+
+
+const elementoCarrusel = document.querySelector('#carruselProductos');
+
+// TRANSICION PARA BOOTSTRAP
+// Hice una variable llamada 'miCarrusel' y le pasé las instrucciones 
+const miCarrusel = new bootstrap.Carousel(elementoCarrusel, {
+
+  // 'interval' es el tiempo que tarda en cambiar de página, medido en milisegundos.
+  // 4000 milisegundos = 4 segundos por cada vista.
+    interval: 4000,
+
+  // 'wrap: true' le dice que cuando llegue al Artículo 6, no se detenga, 
+  // sino que vuelva a empezar desde el Artículo 1 como un ciclo infinito.
+    wrap: true,
+
+
+});
 
